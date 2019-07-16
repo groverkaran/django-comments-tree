27a533a490ab62e2d36f6ec6ba0f545927687e3e
ed@sharpertool.com <ed@sharpertool.com>
2019-07-16 12:04:50 -0600

    Fix issue with 0 in version

--- django_comments_tree/__init__.py
+++ django_comments_tree/__init__.py
@@ -17,12 +17,12 @@ def get_form():
     return import_string(settings.COMMENTS_TREE_FORM_CLASS)
 
 
-VERSION = (0, 1, 0, 'b', 1)  # following PEP 440
+VERSION = (0, 1, 0, 'b', 2)  # following PEP 440
 
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
+    if VERSION[2] is not None:
         version = '%s.%s' % (version, VERSION[2])
     if VERSION[3] != 'f':
         version = '%s%s%s' % (version, VERSION[3], VERSION[4])
